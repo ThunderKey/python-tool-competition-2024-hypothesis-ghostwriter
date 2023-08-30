1eac99f245fd5e0af3f2ccdcc88f14368504e31a
Nicolas Erni <gann@zhaw.ch>
2023-08-30 10:27:07 +0200

    implemented hypothesis ghostwriter generator

--- python_tool_competition_2024_hypothesis_ghostwriter/generator.py
+++ python_tool_competition_2024_hypothesis_ghostwriter/generator.py
@@ -1,11 +1,17 @@
 """A test generator using Hypothesis ghostwriter."""
 
+from hypothesis.extra import ghostwriter
+
 from python_tool_competition_2024.generation_results import (
+    FailureReason,
     TestGenerationFailure,
     TestGenerationResult,
     TestGenerationSuccess,
 )
 from python_tool_competition_2024.generators import FileInfo, TestGenerator
+
+
+_NO_TESTABLE_FUNCTIONS_HEADER = "# Found no testable functions in"
 
 
 class HypothesisGhostwriterTestGenerator(TestGenerator):
@@ -22,4 +28,33 @@ class HypothesisGhostwriterTestGenerator(TestGenerator):
             Either a `TestGenerationSuccess` if it was successful, or a
             `TestGenerationFailure` otherwise.
         """
-        raise NotImplementedError("Implement the test generator")
+        try:
+            # with _add_to_sys_path(target_file_info.):
+            test_body = _build_test_body(target_file_info)
+        except ImportError as error:
+            return TestGenerationFailure(
+                ("The module could not be imported.", str(error)),
+                FailureReason.UNEXPECTED_ERROR,
+            )
+
+        if test_body.startswith(_NO_TESTABLE_FUNCTIONS_HEADER):
+            return TestGenerationFailure(
+                ("Hypothesis Ghostwriter could not find any testable functions.",),
+                FailureReason.NOTHING_GENERATED,
+            )
+
+        # replace common issue
+        test_body = test_body.replace(
+            "st.integers()", "st.integers(min_value=0, max_value=1_000)"
+        )
+
+        return TestGenerationSuccess(test_body)
+
+
+def _build_test_body(target_file_info: FileInfo) -> str:
+    return ghostwriter.magic(
+        target_file_info.import_module(),
+        # *_load_public_functions(target_file_info),
+        style="pytest",
+        annotate=True,
+    )
